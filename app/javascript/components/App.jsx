fdfd345a70f8480125a1042ba872b61158ccb4ee
Shahadat Hossain <shahadat3669@gmail.com>
2023-05-03 13:25:51 +0600

    Add react router dom and update App.jsx file

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Greeting from './Greeting.jsx';
 
 const App = () => {
-  return <Greeting />;
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Greeting />}
+        />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
